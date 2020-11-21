c6aed62f583b350c99faa4dd43591be3e802214c
Rohit Rawat <rohitrawat13396@gmail.com>
2020-11-21 17:00:00 -0500

    Create midikeyboard.py
    1. KeyToMidi: Take an ASCII key as input and return notes[0,11] + octave
    2. MapAsciiToNotes: Map the Ascii values of sequence of characters with notes(0,11)
    3. SetOctave: Update the Octave by (+/- 12)
    4. Encapsulated all of the functions in a class Midikeyboard.

---
+++ PythonOSC/midikeyboard.py
@@ -0,0 +1,34 @@
+class Midikeyboard:
+
+    def __init__(self):
+        self.octave = 48
+        self.dic_ascii = {}
+    
+    #Take the  ASCII value for the keys:('+','-') and update the octet by +12 or -12, respectively
+    #And, rotate the value once it crosses the limit: [0,127]
+    def SetOctave(self,key:int) -> None:
+        if key == 43:
+            self.octave = self.octave + 12 if self.octave<116 else 0
+        elif  key == 45:
+            self.octave = self.octave - 12 if self.octave>11 else 116     
+
+    #Take sequence of characters and update the notes mapping (from 0 to 11)  
+    def MapAsciiToNotes(self,string: str) -> None:
+        ascii_list = [ord(s) for s in string]
+        self.dic_ascii = {}
+        for i,n in enumerate(ascii_list):
+            self.dic_ascii[n] = i        
+
+    #Take an Ascii key as an input and returns the Midi input value.
+    def KeyToMidi(self,key: int) -> int:
+        if key in self.dic_ascii:
+            return self.dic_ascii[key] + self.octave
+        else:
+            return None
+
+obj = Midikeyboard()
+obj.MapAsciiToNotes(",;.lkmjnhbg")
+obj.set_octave(ord('+'))
+obj.set_octave(ord('-'))
+
+print(obj.KeyToMidi(ord(",")))
