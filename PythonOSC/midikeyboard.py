342d232c37a2f3fa4b3f6c36fcf6554949b7ef00
Rohit Rawat <rohitrawat13396@gmail.com>
2020-11-22 22:12:28 -0500

    Update midikeyboard.py
    1. Add transposing function
    2. Made it based on KeyToMidi function

--- PythonOSC/midikeyboard.py
+++ PythonOSC/midikeyboard.py
@@ -3,6 +3,11 @@ class Midikeyboard:
     def __init__(self):
         self.octave = 48
         self.dic_ascii = {}
+        self.transpose = 1
+
+    def Transpose(self):
+        self.transpose *= -1
+        print("transposed!!")
     
     #Take the  ASCII value for the keys:('+','-') and update the octet by +12 or -12, respectively
     #And, rotate the value once it crosses the limit: [0,127]
@@ -21,14 +26,25 @@ class Midikeyboard:
 
     #Take an Ascii key as an input and returns the Midi input value.
     def KeyToMidi(self,key: int) -> list:
+        
         if key in self.dic_ascii:
-            return [self.dic_ascii[key] + self.octave,127]
+            if self.transpose>0:
+                return [self.dic_ascii[key] + self.octave,127]
+            else:
+                return [abs(127 - (self.dic_ascii[key]+self.octave)),127]
+        #ord('+') = 43 and #ord('-') = 45
+        elif key == 43 or key == 45: 
+            self.SetOctave(key)
+        #ord('*') = 42
+        elif key == 42: 
+            self.Transpose()   
         else:
-            return None
+            return None         
 
 obj = Midikeyboard()
 obj.MapAsciiToNotes(",;.lkmjnhbg")
-obj.SetOctave(ord('+'))
-obj.SetOctave(ord('-'))
-
 print(obj.KeyToMidi(ord(",")))
+print(obj.KeyToMidi(ord("*")))
+print(obj.KeyToMidi(ord(",")))
+
+
